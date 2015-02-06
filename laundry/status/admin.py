403f35c9d2ee0a2a449ba0d687bb20428ae4ea45
justathoughtor2 <thoughthaven@gmail.com>
2015-02-05 23:50:53 -0500

    Alter status Response model

--- laundry/status/admin.py
+++ laundry/status/admin.py
@@ -3,7 +3,7 @@ from status.models import Query, Response
 
 class ResponseInline(admin.StackedInline):
     model = Response
-    extra = 2
+    extra = 1
 
 class QueryAdmin(admin.ModelAdmin):
     inlines = [ResponseInline]
--- laundry/status/models.py
+++ laundry/status/models.py
@@ -8,7 +8,6 @@ class Query(models.Model):
 class Response(models.Model):
     query = models.ForeignKey(Query)
     machine_type = models.CharField(max_length=200)
-    in_use = models.BooleanField(default=False)
-    num_machines = models.IntegerField(default=10)
+    machine_in_use = models.IntegerField(default=-1)
     def __unicode__(self):
         return u'%s' % (self.response_text)
