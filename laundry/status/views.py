5175c2ae5a797f2a297dca432fd0b58f47d7b03c
justathoughtor2 <thoughthaven@gmail.com>
2015-02-06 09:39:07 -0500

    I forget, but it was important

--- laundry/status/views.py
+++ laundry/status/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from status.models import Query, Response
 
-# Create your views here.
+def index(request):
+    query_list = Query.objects
+    context = {'query_list': query_list}
+    return render(request, 'status/index.html', context)
+
+def detail(request, query_id):
+    try:
+        query = get_object_or_404(Query, pk=query_id)
+    return render(request, 'status/detail.html', {'query': query})
